eec3a040fbb0adde68130d84b6099638bbccb893
amritpoudel11 <amritpoudel11@gmail.com>
2019-10-20 13:36:21 +0200

    points text changes to red if its negative

--- src/hackstreet/catchit/Points.java
+++ src/hackstreet/catchit/Points.java
@@ -18,13 +18,20 @@ public class Points {
 
         text = new Text(1000,50, "POINTS: 0");
         text.setColor(Color.BLACK);
-        text.grow(80,50);
+        text.grow(60,50);
         text.draw();
     }
 
     public void updatePoints(int points){
         this.points += points;
-        text.setText("POINTS:" + this.points);
+        if (this.points < 0){
+            text.setColor(Color.RED);
+            text.setText("POINTS: " + this.points);
+            return;
+        }
+
+        text.setColor(Color.BLACK);
+        text.setText("POINTS: " + this.points);
     }
 
     public int getPoints(){
